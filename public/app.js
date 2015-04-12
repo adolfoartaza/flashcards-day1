700341a301f643c571ce52f07463b89ce1282902
adolfoartaza <adolfoartaza@gmail.com>
2015-04-12 16:36:32 -0400

    Right/Wrong code written
    Determines if answer is right or wrong correctly and shows the
    appropriate message.

--- public/app.js
+++ public/app.js
@@ -2,11 +2,25 @@ var app = angular.module('FlashCards', []);
 
 app.controller('FlashCardController', function($scope){
 	$scope.flashCard = {
-    question: 'What is Angular?',
-    answers: [
-        { text: 'A front-end framework for great power!', correct: true },
-        { text: 'Something lame, who cares, whatever.', correct: false },
-        { text: 'Some kind of fish, right?', correct: false }
-    ]
+	    question: 'What is Angular?',
+	    answers: [
+	        { text: 'A front-end framework for great power!', correct: true },
+	        { text: 'Something lame, who cares, whatever.', correct	: false },
+	        { text: 'Some kind of fish, right?', correct: false }
+	    ]
 	};
+
+	$scope.answerQuestion = function(response) {
+		if (response.correct){
+			$scope.answeredCorrectly = true;
+		} else {
+			$scope.answeredCorrectly = false;
+		}
+
+		$scope.answered = true;
+	};
+
+	$scope.answered = false;	
+	$scope.answeredCorrectly = false;
+
 });
